5ca1d20090773065887b397fe3a638f576f7d2f6
qq1010903229 <1010903229@qq.com>
2021-10-18 15:03:34 +0800

    Add files via upload

--- javascripts/game.js
+++ javascripts/game.js
@@ -500,7 +500,8 @@ function getInfinitied() {return Math.max(player.infinitied + player.infinitiedB
 
 
 function getGalaxyCostScalingStart() {
-    var n = 100 + ECTimesCompleted("eterc5")*5
+	if (player.currentEternityChall == "eterc5") return 0
+    var n = 10 + ECTimesCompleted("eterc5")*5
     if (player.timestudy.studies.includes(223)) n += 7
     if (player.timestudy.studies.includes(224)) n += Math.floor(player.resets/2000)
     return n
@@ -532,20 +533,15 @@ function getGalaxyRequirement() {
     if (player.currentChallenge == "challenge4") amount = 99 + base;
 
     let galaxyCostScalingStart = getGalaxyCostScalingStart()
-    if (player.currentEternityChall == "eterc5") {
-        amount += Math.pow(player.galaxies, 2) + player.galaxies
-    }
-
-
     if ((player.galaxies) >= getGalaxyCostScalingStart()) {
-        amount += Math.pow((player.galaxies) - (galaxyCostScalingStart - 1), 1.5) + (player.galaxies) - (galaxyCostScalingStart - 1)
+        amount += (player.galaxies - galaxyCostScalingStart)*(player.galaxies - galaxyCostScalingStart - 1)
     }
-
+	
     if ((player.galaxies) >= getGalaxyCostScalingStart() * 2.5) {
         // 5 times worse scaling
-        amount += 4 * (Math.pow((player.galaxies) - (galaxyCostScalingStart * 2.5 - 1), 1.5) + (player.galaxies) - (galaxyCostScalingStart * 2.5 - 1))
+        amount += (player.galaxies - galaxyCostScalingStart * 2.5)*(player.galaxies - galaxyCostScalingStart * 2.5 - 1) * 4
     }
-
+	
     if (player.galaxies >= 800) {
         amount = Math.floor(amount * Math.pow(1.001, (player.galaxies-799)))
     }
@@ -657,7 +653,7 @@ function updateDimensions() {
         }
         else document.getElementById("timeBoostLabel").textContent = 'Time Dimension Boost ('+ player.tdBoosts +'): requires ' + timeShiftRequirement.amount + " " + DISPLAY_NAMES[timeShiftRequirement.tier] + " Time Dimensions"
 
-        document.getElementById("tickspeedResetLabel").textContent = 'Tickspeed Boost ('+ player.tickspeedBoosts +'): requires ' + tickspeedShiftRequirement.amount + " " + DISPLAY_NAMES[tickspeedShiftRequirement.tier] + " Dimensions"
+        document.getElementById("tickspeedResetLabel").textContent = tickspeedShiftRequirement.prefix + 'Tickspeed Boost ('+ player.tickspeedBoosts +'): requires ' + tickspeedShiftRequirement.amount + " " + DISPLAY_NAMES[tickspeedShiftRequirement.tier] + " Dimensions"
 
         if (player.currentChallenge == "challenge4" ? player.resets > 2 : player.resets > 3) {
             document.getElementById("softReset").textContent = "Reset the game for a Boost"
@@ -674,6 +670,7 @@ function updateDimensions() {
         if (player.timestudy.studies.includes(226)) extraGals += Math.floor(player.replicanti.gal / 15)
         var galString = ""
         if (player.galaxies >= 800) galString += "Remote Antimatter Galaxies (";
+        else if (player.galaxies >= getGalaxyCostScalingStart() * 2.5 || player.currentEternityChall === "eterc5") galString += "Further Antimatter Galaxies (";
         else if (player.galaxies >= getGalaxyCostScalingStart() || player.currentEternityChall === "eterc5") galString += "Distant Antimatter Galaxies (";
         else galString += "Antimatter Galaxies (";
         galString += player.galaxies;
@@ -877,7 +874,9 @@ function updateCosts() {
     for (var i=1; i<=8; i++) {
 		var dim = player["timeDimension"+i]
 		dim.costAntimatter = Decimal.pow(timeDimCostMultsAntimatter[i], dim.boughtAntimatter).times(timeDimStartCostsAntimatter[i])
-		
+  if (dim.costAntimatter.gte(Number.MAX_VALUE)) {
+      dim.costAntimatter = Decimal.pow(timeDimCostMultsAntimatter[i]*1.5, dim.boughtAntimatter).times(timeDimStartCostsAntimatter[i])
+  }
   if (player.galacticSacrifice.upgrades.includes(11)) dim.costAntimatter =  dim.costAntimatter.div(galUpgrade11())
         document.getElementById("timeMax"+i+"Antimatter").textContent = "Cost: " + shortenDimensions(player["timeDimension"+i].costAntimatter)
     }
@@ -6638,4 +6637,14 @@ setInterval( function() {
 	
 	document.getElementById("scND").innerHTML = "Normal dimension multipliers and tickspeed are dilated "+(player.dilation.active?"twice ":"")+"after " + shorten(Decimal.pow(10,getDilationStart())) +". <br>Normal dimension multipliers are dilated again after "+ shorten(Decimal.pow(10,getDilationStart2()))+". <br>Normal dimension multipliers are dilated again after "+ shorten(Decimal.pow(10,getDilationStart3()))+".";
 	
+	
+        var timeShiftRequirement = getTimeShiftRequirement(0);
+        var tickspeedShiftRequirement = getTickspeedShiftRequirement(0);
+        if (player.tdBoosts < 7) {
+            document.getElementById("timeBoostLabel").textContent = 'Time Dimension Shift ('+ player.tdBoosts +'): requires ' + timeShiftRequirement.amount + " " + DISPLAY_NAMES[timeShiftRequirement.tier] + " Time Dimensions"
+        }
+        else document.getElementById("timeBoostLabel").textContent = 'Time Dimension Boost ('+ player.tdBoosts +'): requires ' + timeShiftRequirement.amount + " " + DISPLAY_NAMES[timeShiftRequirement.tier] + " Time Dimensions"
+
+        document.getElementById("tickspeedResetLabel").textContent = tickspeedShiftRequirement.prefix + 'Tickspeed Boost ('+ player.tickspeedBoosts +'): requires ' + tickspeedShiftRequirement.amount + " " + DISPLAY_NAMES[tickspeedShiftRequirement.tier] + " Dimensions"
+
 }, 100)
